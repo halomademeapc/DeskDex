e4022c3442cf37fb6df91276d1b683af31a7d9a6
Alex Griffith <alex@halomademeapc.com>
2018-03-29 15:11:49 -0400

    Create models and placeholder data

---
+++ DeskDex/Controllers/StationsController.cs
@@ -0,0 +1,76 @@
+﻿using DeskDex.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DeskDex.Controllers
+{
+    public class StationsController : ApiController
+    {
+        public static List<Equipment> EquipmentTypes = new List<Equipment>
+        {
+            new Equipment("Docking Station", "Dell WD-15 USB Type-C Dock for Laptops"),
+            new Equipment("WYSE Terminal", "Thin client for access to the virtual environment"),
+            new Equipment("Phone", "Cisco IP phone system"),
+            new Equipment("Standing Desk","Desk with adjustable height"),
+            new Equipment("Standing Desk (Extra Tall)", "Desk with adjustable height up to XX inches"),
+            new Equipment("USB Power Outlet", "2-outlet access point with USB connectors for charging devices"),
+            new Equipment("Whiteboard", "Erasable surface for notetaking and collaboration"),
+            new Equipment("MirrorOp Display", "Large display enabled for wireless sharing from a laptop"),
+            new Equipment("SmartBoard", "Interactive display for collaboration")
+        };
+
+        public static List<WorkStyle> WorkStyles = new List<WorkStyle> {
+            new WorkStyle("Agile"),
+            new WorkStyle("Unassigned Resident"),
+            new WorkStyle("Assigned Resident")
+        };
+
+        public static List<Checkin> Checkins = new List<Checkin>
+        {
+            new Checkin
+            {
+                Username = "IFB\agriffith",
+                LastUpdate = DateTime.Now
+            }
+        };
+
+        public static List<Station> Stations = new List<Station>
+        {
+            new Desk
+            {
+                PhysicalAddress = System.Net.NetworkInformation.PhysicalAddress.Parse("28-16-AD-58-D8-33"),
+                    Location = "N-4-D621",
+                    Equipment = new List<Equipment>
+                    {
+                        EquipmentTypes[0],
+                        EquipmentTypes[2],
+                        EquipmentTypes[3]
+                    },
+                    Type = WorkStyles[0],
+                    LastCheckin = Checkins[0]
+            }
+        };
+
+        public IEnumerable<Station> GetStations()
+        {
+            return Stations;
+        }
+
+        public IHttpActionResult GetStation(int ID)
+        {
+            var Station = Stations.FirstOrDefault((s) => s.ID == ID);
+            if (Station == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Station);
+            }
+        }
+    }
+}
---
+++ DeskDex/Models/Checkin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+
+namespace DeskDex.Models
+{
+    public class Checkin
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Last Check-in")]
+        public DateTime LastUpdate { get; set; }
+    }
+    public class Station
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "MAC Address")]
+        public PhysicalAddress PhysicalAddress { get; set; }
+
+        [Display(Name = "Station Number")]
+        public string Location { get; set; }
+
+        [Display(Name = "Available Equipment")]
+        public List<Equipment> Equipment { get; set; }
+
+        public Checkin LastCheckin { get; set; }
+    }
+
+    public class Desk : Station
+    {
+        [Display(Name = "Type")]
+        public WorkStyle Type { get; set; }
+    }
+
+    public class ConferenceRoom : Station
+    {
+        public int Capacity { get; set; }
+    }
+
+    public class WorkStyle
+    {
+        public int ID { get; set; }
+        public WorkStyle(string Name)
+        {
+            this.Name = Name;
+        }
+        public string Name { get; set; }
+    }
+
+    public class Equipment
+    {
+        public int ID { get; set; }
+        public Equipment(string Name, string Description)
+        {
+            this.Name = Name;
+            this.Description = Description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
