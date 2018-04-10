17eee50158912dc8f7e77d21001312b598d938bd
halomademeapc <opr9029@gmail.com>
2018-04-10 18:53:56 -0400

    Switch to Entity Framework Core

--- DeskDexCore/Controllers/EquipmentController.cs
+++ DeskDexCore/Controllers/EquipmentController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using DeskDexCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
--- DeskDexCore/Controllers/WorkStylesController.cs
+++ DeskDexCore/Controllers/WorkStylesController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using DeskDexCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DeskDexCore.Controllers
 {
