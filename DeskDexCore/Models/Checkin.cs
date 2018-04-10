58a010102594d71f2c2e3ad2882d01efa7dae734
halomademeapc <opr9029@gmail.com>
2018-04-10 16:54:52 -0400

    Create new project, migrate models

---
+++ DeskDexCore/Models/Checkin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskDexCore.Models
+{
+    public class Checkin
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Last Check-in")]
+        public DateTime LastUpdate { get; set; }
+    }
+}
---
+++ DeskDexCore/Models/DeskContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskDexCore.Models
+{
+    public class DeskContext : DbContext
+    {
+        public DbSet<Checkin> Checkins { get; set; }
+        public DbSet<Station> Stations { get; set; }
+        public DbSet<WorkStyle> WorkStyles { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+    }
+}
