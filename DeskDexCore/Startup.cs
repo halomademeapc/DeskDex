5dd3e9a727f29bbce420930560ae5412ea119821
halomademeapc <opr9029@gmail.com>
2018-04-13 20:07:02 -0400

    import test page from sample code

--- DeskDexCore/Startup.cs
+++ DeskDexCore/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeskDexCore.Helpers;
 using DeskDexCore.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
